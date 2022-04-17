774cfa06192befaf6b3d0d69d15fbc1579a80f09
lucas kumagai <lucas.kumagai@edu.pucrs.br>
2022-04-17 10:36:07 -0300

    First commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+# Lucas Yugi Kumagai 21110496
+# Gustavo da Nóbrega Silva 21100654
+
+
+# O programa foi criado basicamente em cima do programa do prof. 
+
+# logo que se roda já é mostrado o código json 
+
+# foi completado o desafio 2, assim sendo hospedado a API no server do heroku que pode ser acessado, 
+# por este link https://animes2020.herokuapp.com/
+
+# foi feita 3 tentativas para hospedagem no heroku 2 por git e outra por git hub, tivemos problema no server do heroku 
+# e também na primeira e segunda criação feita por git e a terceira por git hub somente na 4 tentativa por git funcionou, 
+# assim existem vários commits e 1 site a mais no heroku denominado anime2020
+# dificuldades na maneira como manipular para fazer as mudanças no data set 
--- anime.py
+++ anime.py
@@ -1,3 +1,6 @@
+# Lucas Yugi Kumagai 21110496
+# Gustavo da Nóbrega Silva 21100654
+
 import pandas as pd
 from flask import Flask
 import flask.scaffold
@@ -9,18 +12,21 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
+#leitura csv 
 Animes = pd.read_csv('anime.csv')
+#transformação para dicionário orientado a index
 Animes = Animes.to_dict(orient="index")
 
+#para abortar se não possuir nada 
 def abort_if_anime_doesnt_exist(anime_id):
     if anime_id not in Animes:
         abort(404, message=f'Anime {anime_id} doesn\'t exist')
 
 parser = reqparse.RequestParser()
-#parser.add_argument('population', type=int)
 parser.add_argument('MAL_ID', type=int)
 parser.add_argument('Name')
 
+#métodos get, delet, put que retornam um json  
 class Anime(Resource):
     def get(self, anime_id):
         abort_if_anime_doesnt_exist(anime_id)
@@ -37,6 +43,7 @@ class Anime(Resource):
         Animes[anime_id] = content
         return json.dumps(content), 201
 
+#métodos get e post
 class AnimeList(Resource):
     def get(self):
         return json.dumps(Animes)
@@ -47,6 +54,7 @@ class AnimeList(Resource):
         Animes[anime_id] = {'MAL_ID': args['MAL_ID'], 'Name': args['Name']}
         return json.dumps(Animes[anime_id]), 201
 
+#para navegar entre paginas do site, sendo a primeira pagina o json
 api.add_resource(AnimeList, '/')
 api.add_resource(Anime, '/anime/<int:anime_id>')
 
@@ -55,5 +63,7 @@ api.add_resource(Anime, '/anime/<int:anime_id>')
 if __name__ == "__main__":
     app.run(debug=True)
 
-#server do heroku
+#hospedagem no server do heroku
 
+# foi completo o desafio 2, assim sendo hospedado a API no server do heroku que pode ser acessado, 
+# por este link https://animes2020.herokuapp.com/
